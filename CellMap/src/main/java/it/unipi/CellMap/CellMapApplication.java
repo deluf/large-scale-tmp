12f1784133118b75a3c01761f2b09e95aa8aec32
deluf <67149013+deluf@users.noreply.github.com>
2025-08-28 19:14:50 +0200

    Initial commit

---
+++ CellMap/src/main/java/it/unipi/CellMap/CellMapApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.CellMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellMapApplication.class, args);
+	}
+
+}
---
+++ CellMap/src/test/java/it/unipi/CellMap/CellMapApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.CellMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
